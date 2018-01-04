4ba5c29e85dd5d50d3458f8850128d7fd5907412
Ian Huang <ian.huang@dyn-160-39-132-248.dyn.columbia.edu>
2018-01-04 09:10:12 +0800

    update

--- NPLearner.py
+++ NPLearner.py
@@ -7,7 +7,45 @@ import nltk
 import numpy as np
 import pandas as pd
 
-TO_NER = "entity-annotated-corpus/ner_dataset.csv"
+
+POS_PENNBANK = "treebank/tagged"
+NP_PENNBANK = "treebank/combined"
+
+class VanillaNPLearner:
+	"""
+	A vanilla version of NP learner. Purely using the ntlk packages
+	""" 
+
+	def __init__(self, data):
+		"""
+		Inputs:
+			data (np.array): 
+		"""
+
+		## splitting dataset into training and testing
+
+		pass
+
+
+	def fit(self):
+		pass # for consistencey with NPLearner
+
+	def get_features(self):
+		pass # for consistency with NPLearner
+
+	def predict(self):
+		"""
+		Input:
+			POS tags
+		Output:
+			NP labels
+		"""
+
+
+	def evaluate(self):
+		"""
+
+		"""
 
 class NPLearner:
 	def __init__(self):
@@ -24,6 +62,22 @@ class NPLearner:
 
 
 def main():
-	dataset = pd.read_csv(open(TO_NER, mode="r", encoding="ascii", errors="ignore"))
+
+	# getting data into list of lists of tuples.
 	
+	# for every file
+
+		with open(POS_PENNBANK) as f:
+			for line in f:
+				
+
+
+
+
+def clean_data(dataset):
+
+
+
+if __name__ == "__main__":
+	main()
 
