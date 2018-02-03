7f14713d5c59282b5c600f7e754480a071144bcd
Ian Huang <ian.huang@dyn-160-39-132-248.dyn.columbia.edu>
2018-02-03 17:47:05 -0500

    Changes due to retab in vim

--- worldbuilder.py
+++ worldbuilder.py
@@ -1,18 +1,20 @@
 import nltk
 
 class WorldBuilder:
-	"""
-	A class that builds the relations of objects in relation to eachother
-	using semantics extraction and NLP.
-	"""
-	def __init__(self, text):
+        """
+        A class that builds the relations of objects in relation to eachother
+        using semantics extraction and NLP.
+        """
+        def __init__(self, text):
 
-		self.desc = text
+                self.desc = text
 
-	def build(self):
-		"""
-		Implements relations extraction from self.text
-		"""
-		pass
+        def build(self):
+                """
+                Implements relations extraction from self.text
+                """
+                
+                
+                pass
 
-	
+                
